e0f582d9bbd96e3dd854c84d882d790ff8962da8
hmasood21955 <hmasood878@gmail.com>
2023-10-22 15:31:30 +0500

    MONTHLY INTERNEST

--- retire.c
+++ retire.c
@@ -9,3 +9,11 @@ int main(int argc,char *argv[])
     float annual_rate_of_inflation = atof(argv[4]);
     float years_until_retirement = atof(argv[5]);
 
+    double balance = initial_balance;
+    double inflation_adj_interest, new_balance;
+
+    double monthly_rate_of_return = annual_rate_of_return / 12.0;
+    double monthly_rate_of_inflation = annual_rate_of_inflation / 12.0;
+
+
+
